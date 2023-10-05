27b3fd5d726353f6083b3015f1d78d93034a7319
Michael Bonner <michaelwbonner@gmail.com>
2023-10-05 16:05:36 -0600

    Little layout update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<div class="grid gap-8 lg:gap-12">
+<div class="grid gap-8 lg:gap-12 py-8">
 	<header class="w-full max-w-7xl mx-auto px-8">
 		<h1 class="text-3xl font-bold underline">Route Picker</h1>
 	</header>
@@ -12,6 +12,11 @@
 	</main>
 
 	<footer class="w-full max-w-7xl mx-auto px-8 font-light text-sm">
-		<p>&copy;{new Date().getFullYear()} Michael Bonner</p>
+		<p>
+			&copy;{new Date().getFullYear()}
+			<a class="underline underline-offset-2" href="https://michaelbonner.dev" target="_blank"
+				>Michael Bonner</a
+			>
+		</p>
 	</footer>
 </div>
