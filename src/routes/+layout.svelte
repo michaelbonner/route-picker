39e64b19e151e59ca845563f9184f017c4eb3309
Michael Bonner <michaelwbonner@gmail.com>
2023-10-09 23:00:15 -0600

    make h1 a link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 
 <div class="grid gap-8 lg:gap-12 py-8">
 	<header class="w-full max-w-7xl mx-auto px-8">
-		<h1 class="text-3xl font-bold underline">Route Picker</h1>
+		<h1 class="text-3xl font-bold underline">
+			<a href="/">Route Picker</a>
+		</h1>
 	</header>
 
 	<main class="w-full max-w-7xl mx-auto px-8">
