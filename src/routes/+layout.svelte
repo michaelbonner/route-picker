d59b344f72ec636c3c8c864e9edda9ff9aeed6f9
Michael Bonner <michaelwbonner@gmail.com>
2023-10-11 10:03:00 -0600

    add icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,13 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+</svelte:head>
+
 <div class="grid gap-8 lg:gap-12 py-8">
 	<header class="w-full max-w-7xl mx-auto px-8 flex justify-between">
 		<h1 class="text-3xl font-bold underline">
