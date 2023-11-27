1399560f1db639fd7fe8188dedf48d0082548ddc
Michael Bonner <michaelwbonner@gmail.com>
2023-11-26 22:01:00 -0700

    add og:type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 	<meta property="og:image" content="https://www.whichrouteisfaster.com/og-image.jpg" />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
+	<meta property="og:type" content="website" />
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
