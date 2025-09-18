184973025915cc3f1e2fcf9e058972776aebf6db
Michael Bonner <mike@bootpackdigital.com>
2025-09-18 16:13:53 -0600

    Remove preview hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	preview: {
-		allowedHosts: ['whichrouteisfaster.com', 'www.whichrouteisfaster.com']
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
