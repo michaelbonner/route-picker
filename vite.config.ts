8aad6d2db1157a95ea240f2288353f0153263f65
Michael Bonner <mike@bootpackdigital.com>
2025-09-18 15:01:42 -0600

    Add allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	preview: {
+		allowedHosts: ['whichrouteisfastercom-app-movmvi-b03c87-5-78-158-160.traefik.me']
+	}
 });
