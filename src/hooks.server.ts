9c59a933d4b02e2c16ca1ecbbb437f23a7b7a54d
Michael Bonner <mike@bootpackdigital.com>
2026-08-05 11:40:43 -0600

    fix TypeScript 7 type checking

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,9 @@
 import { auth } from '$lib/auth';
+import type { Handle } from '@sveltejs/kit';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 import { building } from '$app/environment';
 
-export async function handle({ event, resolve }) {
+export const handle: Handle = async ({ event, resolve }) => {
 	// Fetch current session from Better Auth
 	const session = await auth.api.getSession({
 		headers: event.request.headers
@@ -13,4 +14,4 @@ export async function handle({ event, resolve }) {
 		event.locals.user = session.user;
 	}
 	return svelteKitHandler({ event, resolve, auth, building });
-}
+};
